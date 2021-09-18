d57c1001417fade517331800ab65f255cbb116b9
Evan Seeyave <eseeyave@mit.edu>
2021-09-18 15:01:04 -0400

    More template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'hi'
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
