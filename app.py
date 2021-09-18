c57a825187040f85fb785785b1e1f235c60abe32
aryo <aryopatel@gmail.com>
2021-09-18 16:48:20 -0400

    Add navbar and outline for  pages

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+@app.route('/events')
+def events():
+	return render_template('events.html')
+
+@app.route('/my-events')
+def my_events():
+	return render_template('my_events.html')
+
+@app.route('/news')
+def news():
+	return render_template('news.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
